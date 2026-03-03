a1dbb21b695a02f14f19bd418a1af78b70ab39a2
sussybaguette <n00255391@iadt.ie>
2026-03-03 13:51:46 +0000

    added more to java

--- src/exercises/06-js-classes-objects/AnimalKingdom/app.js
+++ src/exercises/06-js-classes-objects/AnimalKingdom/app.js
@@ -3,23 +3,18 @@ import Dog from './classes/Dog.js';
 import Wolf from './classes/Wolf.js';
 import Lion from './classes/Lion.js';
 
-let cat1 = new Cat("Tom", 2);
-let dog1 = new Dog("Rover", 2);
+let cat = new Cat("Tom", 2);
+let dog = new Dog("Rover", 2);
 let wolf = new Wolf("Wolfie", 4);
 let lion = new Lion("Simba", 3);
- 
-cat1.makeNoise();
-cat1.roam();
-cat1.sleep();
- 
-dog1.makeNoise();
-dog1.roam();
-dog1.sleep();
 
-wolf.makeNoise();
-wolf.roam();
-wolf.sleep();
+let animals = [cat, dog, lion, wolf];
 
-lion.makeNoise();
-lion.roam();
-lion.sleep();
+animals.forEach((animal) =>{
+    animal.makeNoise();
+    animal.roam();
+    animal.sleep();
+    console.log("---------");
+});
+
+console.log(dog instanceof Dog);
